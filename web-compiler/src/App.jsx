e28e8b6612cc75cf5c243713b18ec89d924f2622
deekshith.v@borderfree.io-at-297767201074 <deekshith.v@gmail.com>
2023-10-09 13:45:39 +0530

    commit1

---
+++ web-compiler/src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css'
+import React, { useEffect, useState } from 'react'
+import Editor from './components/editor.jsx'
+
+export default function App() {
+  const [html, setHtml] = useState('')
+  const [css, setCss] = useState('')
+  const [java, setJava] = useState('')
+  const [srcDoc, setSrcDoc] = useState('')
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+  <html>
+      <body>${html}</body>
+      <style>${css}</style>
+      <script>${java}</script>
+    </html> 
+  `)
+    },250)
+    return()=> clearTimeout(timeout)
+  },[html, css, java])
+
+  return (
+    <div>
+      <section className='editor-div'>
+        <Editor language="xml" displayname="HTML" code={html} onChange={setHtml} />
+        <Editor language="css" displayname="CSS" code={css} onChange={setCss} />
+        <Editor language='javascript' displayname="JavaScript" code={java} onChange={setJava} />
+      </section>
+      <section className='output-div'>
+        <p>Output</p>
+        <iframe
+          className='output-iframe'
+          srcDoc={srcDoc}
+          title='output'
+          sandbox='allow-scripts'>
+        </iframe>
+      </section>
+    </div>
+  )
+}
